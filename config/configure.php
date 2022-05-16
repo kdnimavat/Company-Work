c3a113ad467a82c28dd73c92a7002ee65a8602f9
kdnimavat <101186221+kdnimavat@users.noreply.github.com>
2022-05-16 15:27:40 +0530

    Add files via upload

---
+++ config/configure.php
@@ -0,0 +1,8 @@
+<?php
+
+define ('BASE_PATH', '/jasmin/blog/view/');
+define('DEFAULT_CONTROLLER', '/jasmin/blog/controller/');
+// define('BLOG_DETAIL', '/jasmin/blog/view/blogDetail/');
+// define ('DEFAULT_CONTROLLER', '/controller/RouteController');
+// define ('DEFAULT_ACTION', 'view/user/home');
+?>
